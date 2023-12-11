b03704d5313df5f1a73f652c4ef8b47e05373820
Ivan Ryzhenkov <ocodeok@gmail.com>
2023-12-11 23:01:57 +0300

    Step 5. Enumeration and f-strings

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 todos = ["clean", "prepare", "learn"]
 while True:
-    userAction = input("Type add, show. exit or exit: ")
+    userAction = input("Type add, show. exit, complete or exit: ")
     userAction = userAction.strip()
 
     match userAction:
@@ -9,13 +9,16 @@ while True:
             todo = input("Enter a todo: ")
             todos.append(todo.capitalize())
         case "show":
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print(f"[№ {index}] {item}")
         case "edit":
-            number = int(input("Number of todo to edit: "))
+            number = int(input("Number of the todo to edit: "))
 
             newTodo = input("Enter a new todo: ")
             todos[number] = newTodo
+        case "complete":
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number)
         case "exit":
             break
         case whatever:
