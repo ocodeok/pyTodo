a8195f1eeefe9a27d1cb660d17890a771b243d71
Ivan Ryzhenkov <ocodeok@gmail.com>
2023-12-12 23:32:51 +0300

    If-Elif-Else and Dictionaries & fix bugs

--- main.py
+++ main.py
@@ -6,50 +6,49 @@ while True:
     userAction = input("\nType add, show. exit, complete or exit: ")
     userAction = userAction.strip()
 
-    match userAction:
-        case "add":
-            todo = input("Enter a todo: ") + "\n"
+    if "add" in userAction:
+        todo = userAction[4:] + "\n"
 
-            with open("files/tasks.tsk", "r") as file:
-                todos = file.readlines()
+        with open("files/tasks.tsk", "r") as file:
+            todos = file.readlines()
 
-            todos.append(todo.capitalize())
+        todos.append(todo.capitalize())
 
-            with open("files/tasks.tsk", "w") as file:
-                file.writelines(todos)
-        case "show":
-            with open("files/tasks.tsk", "r") as file:
-                todos = file.readlines()
+        with open("files/tasks.tsk", "w") as file:
+            file.writelines(todos)
+    elif "show" in userAction:
+        with open("files/tasks.tsk", "r") as file:
+            todos = file.readlines()
 
-            for index, item in enumerate(todos):
-                print(f"[№ {index}] {item}", end="")
-        case "edit":
-            with open("files/tasks.tsk", "r") as file:
-                todos = file.readlines()
+        for index, item in enumerate(todos):
+            print(f"[№ {index}] {item}", end="")
+    elif "edit" in userAction:
+        with open("files/tasks.tsk", "r") as file:
+            todos = file.readlines()
 
-            number = int(input("Number of the todo to edit: "))
+        number = int(userAction[5:])
 
-            newTodo = input("Enter a new todo: ")
-            todos[number] = newTodo + "\n"
+        newTodo = input("Enter a new todo: ")
+        todos[number] = newTodo + "\n"
 
-            with open("files/tasks.tsk", "w") as file:
-                file.writelines(todos)
-        case "complete":
-            with open("files/tasks.tsk", "r") as file:
-                todos = file.readlines()
+        with open("files/tasks.tsk", "w") as file:
+            file.writelines(todos)
+    elif "complete" in userAction:
+        with open("files/tasks.tsk", "r") as file:
+            todos = file.readlines()
 
-            number = int(input("Number of the todo to complete: "))
-            removedTodo = todos[number].strip("\n")
-            todos.pop(number)
+        number = int(userAction[9:])
+        removedTodo = todos[number].strip("\n")
+        todos.pop(number)
 
-            with open("files/tasks.tsk", "w") as file:
-                file.writelines(todos)
+        with open("files/tasks.tsk", "w") as file:
+            file.writelines(todos)
 
-            message = f"Todo {removedTodo} was removed from the list."
-            print(message)
-        case "exit":
-            break
-        case whatever:
-            print("Our product does not support such a command")
+        message = f"Todo {removedTodo} was removed from the list."
+        print(message)
+    elif "exit" in userAction:
+        break
+    else:
+        print("Our product does not support such a command")
 
 print("Thank you for using our service")
