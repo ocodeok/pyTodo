f99f9530f8e822972f9e18b4afc765e2488a8a9f
Ivan Ryzhenkov <ocodeok@gmail.com>
2023-12-13 01:09:44 +0300

    Step 10. Add  Try-Except & fig bugs

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def getTodos():
+    with open("files/tasks.tsk", "r") as file:
+        todos = file.readlines()
+    return todos
 
 # todos = ["clean\n", "prepare\n", "learn\n"]
 todos = []
@@ -6,47 +10,55 @@ while True:
     userAction = input("\nType add, show. exit, complete or exit: ")
     userAction = userAction.strip()
 
-    if "add" in userAction:
-        todo = userAction[4:] + "\n"
+    if userAction.startswith("add"):
+        todo = userAction[4:]
 
-        with open("files/tasks.tsk", "r") as file:
-            todos = file.readlines()
+        todos = getTodos()
 
-        todos.append(todo.capitalize())
+        todos.append(todo.capitalize() + "\n")
 
         with open("files/tasks.tsk", "w") as file:
             file.writelines(todos)
-    elif "show" in userAction:
-        with open("files/tasks.tsk", "r") as file:
-            todos = file.readlines()
+    elif userAction.startswith("show"):
+        todos = getTodos()
 
         for index, item in enumerate(todos):
             print(f"[№ {index}] {item}", end="")
-    elif "edit" in userAction:
-        with open("files/tasks.tsk", "r") as file:
-            todos = file.readlines()
+    elif userAction.startswith("edit"):
+        try:
+            todos = getTodos()
 
-        number = int(userAction[5:])
+            number = int(userAction[5:])
 
-        newTodo = input("Enter a new todo: ")
-        todos[number] = newTodo + "\n"
+            newTodo = input("Enter a new todo: ")
+            todos[number] = newTodo + "\n"
 
-        with open("files/tasks.tsk", "w") as file:
-            file.writelines(todos)
-    elif "complete" in userAction:
-        with open("files/tasks.tsk", "r") as file:
-            todos = file.readlines()
+            with open("files/tasks.tsk", "w") as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Incorrect command form. You only need to enter the task number")
+            continue
+    elif userAction.startswith("complete"):
+        try:
+            try:
+                todos = getTodos()
 
-        number = int(userAction[9:])
-        removedTodo = todos[number].strip("\n")
-        todos.pop(number)
+                number = int(userAction[9:])
+                removedTodo = todos[number].strip("\n")
+                todos.pop(number)
 
-        with open("files/tasks.tsk", "w") as file:
-            file.writelines(todos)
+                with open("files/tasks.tsk", "w") as file:
+                    file.writelines(todos)
 
-        message = f"Todo {removedTodo} was removed from the list."
-        print(message)
-    elif "exit" in userAction:
+                message = f"Todo {removedTodo} was removed from the list."
+                print(message)
+            except IndexError:
+                print("There is no element with this number. Use the 'show' command")
+                continue
+        except ValueError:
+            print("Incorrect command form. You only need to enter the task number")
+            continue
+    elif userAction.startswith("exit"):
         break
     else:
         print("Our product does not support such a command")
