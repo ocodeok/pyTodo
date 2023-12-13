536774af5ea700c2ce33b51f3cf36f9df1d9106a
Ivan Ryzhenkov <ocodeok@gmail.com>
2023-12-13 20:02:39 +0300

    Create GUI add & edit buttons, listbox

--- gui.py
+++ gui.py
@@ -1,27 +1,40 @@
-import PySimpleGUI as sq
+import PySimpleGUI as sg
 
 from modules.functions import getTodos, writeTodos
 
-label = sq.Text("Type in a to-do")
-inputBox = sq.InputText(tooltip="Enter a todo", key="todo")
-addButton = sq.Button("Add")
+label = sg.Text("Type in a to-do")
+inputBox = sg.InputText(tooltip="Enter a todo", key="todo")
+addButton = sg.Button("Add")
+editButton = sg.Button("Edit")
+listBox = sg.Listbox(values=getTodos(), key="todos", enable_events=True, size=(45, 10))
 
-window = sq.Window(
+window = sg.Window(
     "My To-Do App",
-    layout=[[label], [inputBox, addButton]],
+    layout=[[label], [inputBox, addButton], [listBox, editButton]],
     font=("Consolas", 20))
 
 while True:
     event, values = window.read()
-    print(event, values)
+    print(values)
     match event:
         case "Add":
             todos = getTodos()
             newTodo = values['todo'] + "\n"
-            print(newTodo)
             todos.append(newTodo)
             writeTodos(todos)
-        case sq.WINDOW_CLOSED:
+            window['todos'].update(values=todos)
+        case "Edit":
+            selectTodo = values["todos"][0]
+            newTodo = values['todo']
+
+            todos = getTodos()
+            index = todos.index(selectTodo)
+            print(index)
+            todos[index] = newTodo
+            writeTodos(todos)
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
+        case sg.WINDOW_CLOSED:
             break
 
 window.close()
